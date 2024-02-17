4e6852c696bfe24d8a3708471a1a1281112d046c
joashcabanilla <joash0199@gmail.com>
2024-02-17 21:05:49 +0800

    last update for local

--- novadeci-election/admin_nvdc/includes/conn.php
+++ novadeci-election/admin_nvdc/includes/conn.php
@@ -1,6 +1,7 @@
 <?php
-	$conn = mysqli_connect("localhost", "u209391291_mis_votes", "n0v@d3c11976MIS", "u209391291_votesystem");
-
+	// $conn = mysqli_connect("localhost", "u209391291_mis_votes", "n0v@d3c11976MIS", "u209391291_votesystem");
+	
+	$conn = mysqli_connect("localhost", "root", "", "migs");
 	if(!$conn){
 		die("Error: Failed to connect to database!");
 	}
